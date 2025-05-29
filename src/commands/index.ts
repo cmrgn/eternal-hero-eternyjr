2dc27626c67df883209469d3bed2eeafc8b7c6c3
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2025-05-29 16:56:03 +0200

    Remove reload command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,6 @@
 import * as dust from './dust'
 import * as faq from './faq'
 import * as giveaway from './giveaway'
-import * as reload from './reload'
 import * as rule from './rule'
 import * as talk from './talk'
 import * as timeout from './timeout'
@@ -10,7 +9,6 @@ export const commands = {
   dust,
   faq,
   giveaway,
-  reload,
   rule,
   talk,
   timeout,
