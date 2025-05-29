e6bc0197cbddd6076a10463f5edcabd4aab3ceef
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2025-05-29 12:34:15 +0200

    Remove obsolete GitHub token

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,6 @@ if (!process.env.DISCORD_TOKEN || !process.env.DISCORD_CLIENT_ID) {
 }
 
 export const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID
-export const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 export const DATABASE_URL = process.env.DATABASE_URL
 export const DISCORD_TOKEN = process.env.DISCORD_TOKEN
 export const TEST_SERVER_ID = process.env.TEST_SERVER_ID
