44ec9685ccf00c66bbfa825752228297beba1e88
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2025-06-10 17:09:35 +0200

    Improve timeout input

--- src/commands/timeout.ts
+++ src/commands/timeout.ts
@@ -10,17 +10,17 @@ import {
 } from 'discord.js'
 import { logger } from '../utils/logger'
 import { RULES_CHOICES } from './rule'
+import ms, { StringValue } from 'ms'
 
 export const data = new SlashCommandBuilder()
   .setName('timeout')
   .addUserOption(option =>
     option.setName('user').setDescription('User to time out').setRequired(true)
   )
-  .addIntegerOption(option =>
+  .addStringOption(option =>
     option
       .setName('duration')
-      .setDescription('Timeout duration')
-      .setMinValue(1)
+      .setDescription('Timeout duration (e.g. 60s, 5mn, 1hr, 1d)')
       .setRequired(true)
   )
   .addStringOption(option =>
@@ -37,15 +37,17 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction: ChatInputCommandInteraction) {
   const rule = interaction.options.getString('violation', true)
   const member = interaction.options.getMember('user') as GuildMember
-  const duration = interaction.options.getInteger('duration', true)
+  const duration = interaction.options.getString('duration', true)
+  const durationMs = ms(duration as StringValue)
 
   logger.command(interaction)
 
   if (!interaction.guild) throw new Error('Cannot retrieve guild.')
   if (!member) throw new Error('Cannot retrieve member.')
+  if (durationMs === 0) throw new Error('Cannot time out a user for 0ms.')
 
   // Time out the user
-  await member.timeout(duration * 60 * 1_000, `Violating rule ${rule}`)
+  await member.timeout(durationMs, `Violating rule ${rule}`)
 
   // biome-ignore lint/style/noNonNullAssertion: <explanation>
   const channels = interaction.guild!.channels
@@ -54,7 +56,7 @@ export async function execute(interaction: ChatInputCommandInteraction) {
   )
 
   const [number, label] = rule.split(': ')
-  const message = `${userMention(member.id)} was timed out for ${duration} minute${duration === 1 ? '' : 's'} for violating ${bold(number.toLocaleLowerCase())} (${label}).`
+  const message = `${userMention(member.id)} was timed out for ${ms(durationMs)} for violating ${bold(number.toLocaleLowerCase())} (${label}).`
 
   // Announce the timeout
   if (moderation?.isSendable()) await moderation.send(message)
