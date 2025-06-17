1d2a5801773c8ff0db5d1f717f8cc371981feb91
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2025-06-17 11:01:03 +0200

    Avoid an edge case

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,7 +1,13 @@
 import type { Guild } from 'discord.js'
 import { deployCommands } from '../utils/commands'
+import { IS_DEV } from '../config'
 
 export function onGuildCreate(guild: Guild) {
+  // The local bot should never react to adding the bot to other servers, since
+  // the production bot already does that, and we don’t want to duplicate all
+  // the commands.
+  if (IS_DEV) return
+
   // Deploy the commands for the guild (Discord server) when adding the bot to
   // said Discord server.
   return deployCommands(guild.id)
