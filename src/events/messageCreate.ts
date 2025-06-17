ee4bf5c7f631bfc18c2405a58100b1800cfa8cb8
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2025-06-17 10:52:21 +0200

    Disable features in secondary servers

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -11,6 +11,7 @@ import { shouldIgnoreInteraction } from '../utils/shouldIgnoreInteraction'
 import { discordLinking } from './messageCreate.discordLinking'
 import { languageDetection } from './messageCreate.languageDetection'
 import { getChannelFromInteraction } from '../utils/getChannelFromInteraction'
+import { DISCORD_SERVER_ID, TEST_SERVER_ID } from '../config'
 
 export type InteractionLike = OmitPartialGroupDMChannel<Message<boolean>>
 export type EnsuredInteraction = Omit<
@@ -27,6 +28,11 @@ export async function onMessageCreate(interaction: InteractionLike) {
 
   // If the interaction is incomplete, or if it comes from a bot, do nothing.
   if (!guild || !member || member.user.bot || !guild.members.me) return
+
+  // If the bot is running neither on the official server, nor on the testing
+  // server, do nothing since Discord linking and language detection may not
+  // be relevant at all.
+  if (guild.id !== DISCORD_SERVER_ID && guild.id !== TEST_SERVER_ID) return
 
   // If the interaction should be ignored, ignore it.
   if (shouldIgnoreInteraction(interaction)) return
