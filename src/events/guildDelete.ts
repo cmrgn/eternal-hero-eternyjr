ee8527ab32022e2fa810c421d911d95b0c300392
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2025-06-17 11:01:30 +0200

    Remove obsolete file

--- src/events/guildDelete.ts
+++
@@ -1,8 +0,0 @@
-import type { Guild } from 'discord.js'
-import { deleteCommands } from '../utils/commands'
-
-export function onGuildDelete(guild: Guild) {
-  // Remove the commands for the guild (Discord server) when removing the bot
-  // from said Discord server.
-  return deleteCommands(guild.id)
-}
