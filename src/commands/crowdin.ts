f6f645bb89444f232442de13010928a710e7a45c
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2025-06-19 11:57:22 +0200

    Fix issue with long messages

--- src/commands/crowdin.ts
+++ src/commands/crowdin.ts
@@ -194,7 +194,30 @@ ${
 }
   `
 
-  return interaction.editReply({ content })
+  const [response, ...responses] = splitMarkdownList(content)
+  await interaction.editReply({ content: response })
+  for (const response of responses) {
+    await interaction.followUp({ content: response, flags })
+  }
+}
+
+function splitMarkdownList(message: string, maxLength = 2000): string[] {
+  const lines = message.split('\n')
+  const chunks: string[] = []
+  let current = ''
+
+  for (const line of lines) {
+    const candidate = current + (current ? '\n' : '') + line
+    if (candidate.length > maxLength) {
+      if (current) chunks.push(current)
+      current = line
+    } else {
+      current = candidate
+    }
+  }
+
+  if (current) chunks.push(current)
+  return chunks
 }
 
 function formatTranslation({
