83041bdcc80b7e93b0e9c4d313eab2fc27d489fe
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2025-07-03 20:21:11 +0200

    Simplify the /crowdin term command

--- src/managers/CrowdinManager.ts
+++ src/managers/CrowdinManager.ts
@@ -2,7 +2,6 @@ import type { Client } from 'discord.js'
 import {
   default as Crowdin,
   type SourceStringsModel,
-  type LanguagesModel,
 } from '@crowdin/crowdin-api-client'
 import decompress from 'decompress'
 import csvtojson from 'csvtojson'
@@ -10,7 +9,6 @@ import fetch from 'node-fetch'
 
 import { CROWDIN_TOKEN } from '../constants/config'
 import { logger } from '../utils/logger'
-import { pool } from '../utils/pg'
 import type { LocalizationItem } from './LocalizationManager'
 import {
   LANGUAGE_OBJECTS,
@@ -33,10 +31,10 @@ export type CrowdinItem = Record<CrowdinCode, string> & {
 }
 
 export class CrowdinManager {
-  #projectId = 797774
-  #projectIdentifier = 'eternal-hero'
   client: Client
   crowdin: Crowdin
+
+  #projectId = 797774
 
   #cachedTranslations: LocalizationItem[] | null = null
   #lastFetchedAt = 0
@@ -64,35 +62,11 @@ export class CrowdinManager {
     this.#log('info', 'Resolving project')
     const projects = await this.crowdin.projectsGroupsApi.listProjects()
     const project = projects.data.find(
-      project => project.data.identifier === this.#projectIdentifier
+      ({ data: { id } }) => id === this.#projectId
     )
     if (!project) throw new Error('Cannot find Crowdin project.')
 
     return project.data
-  }
-
-  async cacheCrowdinStrings(strings: SourceStringsModel.String[]) {
-    this.#log('info', 'Caching strings')
-
-    const values: string[] = []
-    const params: (string | number | SourceStringsModel.PluralText)[] = []
-
-    strings.forEach((string, i) => {
-      const offset = i * 3
-      values.push(`($${offset + 1}, $${offset + 2}, $${offset + 3})`)
-      params.push(string.identifier, string.id, string.text)
-    })
-
-    if (params.length > 0) {
-      await pool.query(
-        `
-        INSERT INTO crowdin_strings (identifier, string_id, text)
-        VALUES ${values.join(',')}
-        ON CONFLICT (identifier) DO NOTHING
-        `,
-        params
-      )
-    }
   }
 
   async getProjectProgress() {
@@ -104,94 +78,6 @@ export class CrowdinManager {
       )
 
     return projectProgress
-  }
-
-  async getLanguageObject(crowdinCode: CrowdinCode) {
-    this.#log('info', 'Getting language object')
-
-    const { targetLanguages: languageObjects } = await this.getProject()
-
-    return languageObjects.find(
-      languageObject => languageObject.id === crowdinCode
-    )
-  }
-
-  async getStringTranslationsForAllLanguages(stringId: StringId) {
-    this.#log('info', 'Getting all translations from string', { stringId })
-
-    const { targetLanguages: languages } = await this.getProject()
-
-    return this.getStringTranslations(stringId, languages)
-  }
-
-  async getStringTranslations(
-    stringId: StringId,
-    languages: LanguagesModel.Language[]
-  ) {
-    this.#log('info', 'Getting string translations from string', { stringId })
-
-    return Promise.all(
-      languages.map(language =>
-        this.getProjectStringTranslation(stringId, language)
-      )
-    )
-  }
-
-  async getStringItem(identifier: string) {
-    this.#log('info', 'Getting string item', { identifier })
-
-    const { rows } = await pool.query(
-      'SELECT string_id, text FROM crowdin_strings WHERE identifier = $1',
-      [identifier]
-    )
-
-    const result: { string_id: StringId; text: string } | undefined = rows[0]
-
-    if (!result) {
-      const strings = await this.getProjectStrings()
-      await this.cacheCrowdinStrings(strings)
-
-      return strings.find(string => string.identifier === identifier)
-    }
-
-    return { id: result.string_id, text: result.text }
-  }
-
-  async getProjectStringTranslation(
-    stringId: StringId,
-    language: LanguagesModel.Language
-  ) {
-    this.#log('info', 'Getting string translation', { stringId, language })
-
-    const outcome =
-      await this.crowdin.stringTranslationsApi.listStringTranslations(
-        this.#projectId,
-        stringId,
-        language.id
-      )
-
-    return { language, translation: outcome.data[0] }
-  }
-
-  async getProjectStrings() {
-    this.#log('info', 'Getting project strings')
-
-    const allStrings: SourceStringsModel.String[] = []
-    let offset = 0
-    const limit = 500 // Max limit per request
-
-    while (true) {
-      const { data } = await this.crowdin.sourceStringsApi.listProjectStrings(
-        this.#projectId,
-        { limit, offset }
-      )
-
-      allStrings.push(...data.map(item => item.data))
-      if (data.length < limit) break
-      offset += limit
-    }
-
-    return allStrings
   }
 
   async buildProject() {
