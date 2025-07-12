85baffaf508aca764a4c35c97b71248a8e191570
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2025-07-12 22:11:24 +0200

    Remove obsolete utility

--- src/commands/indexfaq.ts
+++ src/commands/indexfaq.ts
@@ -310,17 +310,3 @@ async function commandStats(interaction: ChatInputCommandInteraction) {
 
   return interaction.editReply({ content })
 }
-
-function cleanUpTranslation(string: string) {
-  return (
-    string
-      // Remove line breaks
-      .replace(/\n/g, '')
-      // Replace pluralization tokens with the singular form
-      .replace(/\{0:plural:([^|}]+)\|[^}]+\}/g, (_, singular) => singular)
-      // Remove tags
-      .replace(/<[a-z=]+>/g, '')
-      .replace(/<\/[a-z=]+>/g, '')
-      .trim()
-  )
-}
